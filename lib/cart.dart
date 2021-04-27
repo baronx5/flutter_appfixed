bed77dd715e8dd6feeeb9ed62b887b35651340bd
GEOLOGIST <baron1574@gmil.com>
2021-04-27 21:18:32 +0300

    Edit Cart Design ...

--- lib/cart.dart
+++ lib/cart.dart
@@ -14,97 +14,94 @@ class CartPage extends StatelessWidget {
                 child: ListView.builder(
                     itemCount: cart.basketItems.length,
                     itemBuilder: (context, i) {
-                      return Center(
-                        child: Column(
+                      return Container(
+                        margin: EdgeInsets.all(10.0),
+                        color: Colors.grey[200],
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
                           children: [
                             Container(
-                              margin: EdgeInsets.all(5.0),
-                              padding: EdgeInsets.only(right: 20.0),
-                              //height: 100,
-                              color: Colors.grey[200],
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Container(
-                                    width: 100,
-                                    height: 100,
-                                    color: Colors.lightGreen,
-                                    child: Image.network(
-                                      cart.basketItems[i].productItem.image,
-                                      fit: BoxFit.cover,
+                              width: 100,
+                              height: 100,
+                              color: Colors.lightGreen,
+                              child: Image.network(
+                                cart.basketItems[i].productItem.image,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Container(
+                                height: 100,
+                                width: 100,
+                                //color: Colors.redAccent,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Text(
+                                      cart.basketItems[i].productItem.getAddonsPrice.toString() +
+                                          " KWD",
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          fontFamily: 'Droid',
+                                          fontSize: 14,
+                                          color: Colors.lightGreen),
                                     ),
-                                  ),
-                                  Container(
-                                      height: 100,
-                                      width: 100,
-                                      //color: Colors.redAccent,
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                        children: [
-                                          Text(
-                                            cart.basketItems[i].productItem.getAddonsPrice.toString() +
-                                                " KWD",
-                                            textAlign: TextAlign.left,
-                                            style: TextStyle(
-                                                fontFamily: 'Droid',
-                                                fontSize: 14,
-                                                color: Colors.lightGreen),
-                                          ),
-                                          InkWell(
-                                            child: TextButton(
-                                                child: Text('حذف', style: TextStyle(fontFamily: 'Droid', fontSize: 12),),
-                                                style: ButtonStyle(
-                                                    padding:
-                                                        MaterialStateProperty.all<EdgeInsets>(
-                                                            EdgeInsets.all(5.0)),
-                                                    foregroundColor:
-                                                        MaterialStateProperty.all<
-                                                            Color>(Colors.red),
-                                                    shape: MaterialStateProperty.all<
-                                                            RoundedRectangleBorder>(
-                                                        RoundedRectangleBorder(
-                                                            borderRadius:
-                                                                BorderRadius.circular(18.0),
-                                                            side: BorderSide(color: Colors.red))))),
-                                            onTap: () {
-                                              cart.remove(cart.basketItems[i]);
-                                            },
-                                          )
-                                        ],
-                                      )),
-                                  Expanded(
-                                    child: Column(
-                                      children: [
-                                        Text(
-                                          cart.basketItems[i].productItem.name,
-                                          textAlign: TextAlign.right,
-                                          style: TextStyle(
-                                              fontFamily: 'Droid',
-                                              fontSize: 14,
-                                              color: Colors.grey[700]),
-                                        ),
-                                        Container(
-                                          height: 200,
-                                          child: ListView.builder(
-                                              itemCount: cart.basketItems[i].productItem.addons.length,
-                                              itemBuilder: (context, index){
+                                    InkWell(
+                                      child: TextButton(
+                                          child: Text('حذف', style: TextStyle(fontFamily: 'Droid', fontSize: 12),),
+                                          style: ButtonStyle(
+                                              padding:
+                                              MaterialStateProperty.all<EdgeInsets>(
+                                                  EdgeInsets.all(5.0)),
+                                              foregroundColor:
+                                              MaterialStateProperty.all<
+                                                  Color>(Colors.red),
+                                              shape: MaterialStateProperty.all<
+                                                  RoundedRectangleBorder>(
+                                                  RoundedRectangleBorder(
+                                                      borderRadius:
+                                                      BorderRadius.circular(18.0),
+                                                      side: BorderSide(color: Colors.red))))),
+                                      onTap: () {
+                                        cart.remove(cart.basketItems[i]);
+                                      },
+                                    )
+                                  ],
+                                )),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      cart.basketItems[i].productItem.name,
+                                      textAlign: TextAlign.right,
+                                      style: TextStyle(
+                                          fontFamily: 'Droid',
+                                          fontSize: 14,
+                                          color: Colors.grey[700]),
+                                    ),
+                                    Container(
+                                      height: 50,
+                                      child: ListView.builder(
+                                        shrinkWrap: true,
+                                          scrollDirection: Axis.horizontal,
+                                          itemCount: cart.basketItems[i].productItem.addons.length,
+                                          itemBuilder: (context, index){
 
                                             return Text(
-                                              cart.basketItems[i].productItem.addons[index].name,textAlign: TextAlign.right,
+                                              " : " + cart.basketItems[i].productItem.addons[index].name ,textAlign: TextAlign.right,
                                               style: TextStyle(
                                                   fontFamily: 'Droid',
                                                   fontSize: 12,
                                                   color: Colors.grey[500]),
                                             );
                                           }),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ],
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
                           ],
