ed797b2e715fc252a34e0429952872860cba8f49
GEOLOGIST <baron1574@gmil.com>
2021-04-30 02:11:32 +0300

    New directory for Login.

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -9,7 +9,6 @@ class LoginPage extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       //appBar: CustomAppBar(),
       body: Container(
-
         decoration: BoxDecoration(
           image: DecorationImage(
             image: NetworkImage('https://images.unsplash.com/photo-1577670552647-6ce12a463e7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80'),
@@ -23,6 +22,7 @@ class LoginPage extends StatelessWidget {
             SizedBox(
               height: 100,
             ),
+            Image.network('https://flutterforweb.000webhostapp.com/resturant/logo.jpg', width: 150,),
             Text('تسجيل الدخول',style: TextStyle(color: Colors.black, fontSize: 24, fontFamily: 'Droid')),
             Container(
               width: 300,
@@ -54,6 +54,9 @@ class LoginPage extends StatelessWidget {
               child: Text('تسجيل الدخول',style: TextStyle(color: Colors.white,fontFamily: 'Droid', fontSize: 16)),
               onPressed: () {  }),
             Text('نسيت الرقم السري',style: TextStyle(color: Colors.black,fontFamily: 'Droid', fontSize: 16)),
+            InkWell(child: Text('تسجيل لاحقاً',style: TextStyle(color: Colors.black,fontFamily: 'Droid', fontSize: 16)),onTap: (){
+              Navigator.pushReplacementNamed(context, 'MyApp');
+            },),
             SizedBox(
               height: 100,
             )
