7a00bedac7b6278d4c563fda38a42f81705b91ae
Hussain SK <robinx5.q8@gmail.com>
2021-05-09 02:02:28 +0300

    update

--- lib/apiResponse.dart
+++ lib/apiResponse.dart
@@ -124,7 +124,8 @@ Future placeOrder(User user, List<Item> orders, BuildContext context) async {
 
 
 Future newAddress( User user, BuildContext context) async {
-  var data = {"userId":user.id.toString(), "address": user.address.toJson()};
+
+  var data = {"address": json.encode(user.address.toJson())};
   var url = apiUrl + '/address/add/';
   var response = await http.post(Uri.parse(url), body: data);
   var responseBody = jsonDecode(response.body);
