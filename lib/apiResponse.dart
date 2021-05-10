3485c085805fc73650b6378169e79571061179c2
Hussain SK <robinx5.q8@gmail.com>
2021-05-10 23:28:04 +0300

    update ip in api

--- lib/apiResponse.dart
+++ lib/apiResponse.dart
@@ -12,9 +12,9 @@ import 'Models/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 // Url https://flutterforweb.000webhostapp.com/
-// Url http://127.0.0.1:5000/
-// Url http://10.0.2.2:5000/
 String apiUrl = "http://10.0.2.2:5000/";
+// String apiUrl = "http://10.0.2.2:5000/";
+// String apiUrl = "https://flutterforweb.000webhostapp.com/";
 
 Future getSettingsData() async {
   var url = Uri.parse(apiUrl + "settings");
