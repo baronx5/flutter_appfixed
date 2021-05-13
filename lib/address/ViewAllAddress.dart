50a9c4ed9b6c6751d6c44e6b6e1dae34077e18bf
GEOLOGIST <baron1574@gmil.com>
2021-05-13 19:16:01 +0300

    checkout Address fixing

--- lib/address/ViewAllAddress.dart
+++ lib/address/ViewAllAddress.dart
@@ -6,15 +6,11 @@ import 'package:flutter_appfixed/Models/user.dart';
 import 'package:flutter_appfixed/address/editAddress.dart';
 
 class ViewAllAddress extends StatefulWidget {
-
-
   @override
   _ViewAllAddressState createState() => _ViewAllAddressState();
 }
 
 class _ViewAllAddressState extends State<ViewAllAddress> {
-
-
   @override
   Widget build(BuildContext context) {
     final User user = ModalRoute.of(context).settings.arguments;
@@ -22,7 +18,7 @@ class _ViewAllAddressState extends State<ViewAllAddress> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'title',
+          'اختر العنوان المناسب',
           style: TextStyle(fontFamily: 'Droid', color: Colors.black54),
         ),
         centerTitle: true,
@@ -30,31 +26,68 @@ class _ViewAllAddressState extends State<ViewAllAddress> {
         elevation: 0,
         iconTheme: IconThemeData(color: Colors.black54),
       ),
-      body: FutureBuilder<List<Address>>(
-          future: getAddressData(user.id),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(snapshot.data[index].area),
-                      leading: Icon(Icons.six_ft_apart),
-                      onTap: () {
-
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => EditAddress(user)),
-                        );
-                      },
-                    );
-                  });
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-            return CircularProgressIndicator();
-          }),
+      body: ListView(
+        padding: EdgeInsets.all(20.0),
+        children: [
+          SizedBox(
+            height: 200,
+            child: Image.network('https://media.istockphoto.com/vectors/quarantine-coronavirus-epidemic-free-delivery-man-in-a-protective-vector-id1214019831?k=6&m=1214019831&s=612x612&w=0&h=U1ZweEl62C9nZRwdv8JV7MgA-36d6eaUMf88yW9soHY='),
+          ),
+          FutureBuilder<List<Address>>(
+              future: getAddressData(user.id),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, index) {
+                        return new Directionality(
+                            textDirection: TextDirection.rtl,
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  contentPadding: EdgeInsets.all(0),
+                                  title: Row(
+                                    children: [
+                                      Text(snapshot.data[index].area,style: TextStyle(fontFamily: 'Droid', fontSize: 14),),
+                                      Text(" ق " + snapshot.data[index].block,style: TextStyle(fontFamily: 'Droid', fontSize: 14),),
+                                      Text(" ش " +snapshot.data[index].street,style: TextStyle(fontFamily: 'Droid', fontSize: 14),),
+                                      Text(" ج " +snapshot.data[index].jada,style: TextStyle(fontFamily: 'Droid', fontSize: 14),),
+                                      Text(" د " +snapshot.data[index].floor,style: TextStyle(fontFamily: 'Droid', fontSize: 14),),
+                                      Text(" م " +snapshot.data[index].houseNumber,style: TextStyle(fontFamily: 'Droid', fontSize: 14),),
+                                    ],
+                                  ),
+                                  leading: Icon(Icons.location_city),
+                                  trailing: Icon(
+                                    Icons.edit,
+                                    color: Colors.orange,
+                                  ),
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => EditAddress(user)),
+                                    );
+                                  },
+                                ),
+                                Divider()
+                              ],
+                            ));
+                      });
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return CircularProgressIndicator();
+              }),
+          SizedBox(
+            height: 50,
+            child: ElevatedButton(onPressed: (){},
+                style: ElevatedButton.styleFrom(primary: Colors.orange,
+                ),
+                child: Text('اضف عنوان جديد', style: TextStyle(fontFamily: 'Droid', fontSize: 16),)),
+          )
+        ],
+      )
     );
   }
 }
