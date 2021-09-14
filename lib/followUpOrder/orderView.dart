3babb9a4d25a1e2ac19ad89d07a33544dbcbf6ac
GEOLOGIST <baron1574@gmil.com>
2021-09-14 21:51:19 +0300

    added list View for orverview page.

--- lib/followUpOrder/orderView.dart
+++ lib/followUpOrder/orderView.dart
@@ -21,128 +21,130 @@ class OrderView extends StatelessWidget {
         elevation: 0,
         iconTheme: IconThemeData(color: Colors.black54),
       ),
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'طلب رقم : ${orderId.toString()}',
-              style: TextStyle(fontFamily: 'Droid', fontSize: 22),
-            ),
-            Text(
-              'توقيت الطلب : ١٠.١٥',
-              style: TextStyle(fontFamily: 'Droid', fontSize: 22),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(20.0),
-                      margin: EdgeInsets.all(10),
-                      child: Icon(Icons.delivery_dining),
-                      decoration: BoxDecoration(
-                          color: orderDetails.level == 'delivery' ? Colors.orange[400] : Colors.grey[200], shape: BoxShape.circle),
-                    ),
-                    Text('جاري توصيل الطلب',
-                        style: TextStyle(fontFamily: 'Droid')),
-                  ],
-                ),
-                Container(
-                  color: Colors.grey,
-                  child: Container(
-                    width: 40,
-                    height: 1,
-                    padding: EdgeInsets.all(10),
+      body: ListView(
+        children:[Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'طلب رقم : ${orderId.toString()}',
+                style: TextStyle(fontFamily: 'Droid', fontSize: 22),
+              ),
+              Text(
+                'توقيت الطلب : ١٠.١٥',
+                style: TextStyle(fontFamily: 'Droid', fontSize: 22),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(20.0),
+                        margin: EdgeInsets.all(10),
+                        child: Icon(Icons.delivery_dining),
+                        decoration: BoxDecoration(
+                            color: orderDetails.level == 'delivery' ? Colors.orange[400] : Colors.grey[200], shape: BoxShape.circle),
+                      ),
+                      Text('جاري توصيل الطلب',
+                          style: TextStyle(fontFamily: 'Droid')),
+                    ],
                   ),
-                ),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(20.0),
-                      margin: EdgeInsets.all(10),
-                      child: Icon(Icons.fastfood_outlined),
-                      decoration: BoxDecoration(
-                          color: orderDetails.level == 'cooking'|| orderDetails.level == 'delivery' ? Colors.orange[400] : Colors.grey[200], shape: BoxShape.circle),
+                  Container(
+                    color: Colors.grey,
+                    child: Container(
+                      width: 40,
+                      height: 1,
+                      padding: EdgeInsets.all(10),
                     ),
-                    Text('جاري تحضير الطلب',
-                        style: TextStyle(fontFamily: 'Droid')),
-                  ],
-                ),
-                Container(
-                  color: Colors.grey,
-                  child: Container(
-                    width: 40,
-                    height: 1,
-                    padding: EdgeInsets.all(10),
                   ),
-                ),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(20.0),
-                      margin: EdgeInsets.all(10),
-                      child: Icon(Icons.done),
-                      decoration: BoxDecoration(
-                          color: orderDetails.level == 'new' || orderDetails.level == 'cooking'|| orderDetails.level == 'delivery'  ? Colors.orange[400] : Colors.grey[200], shape: BoxShape.circle),
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(20.0),
+                        margin: EdgeInsets.all(10),
+                        child: Icon(Icons.fastfood_outlined),
+                        decoration: BoxDecoration(
+                            color: orderDetails.level == 'cooking'|| orderDetails.level == 'delivery' ? Colors.orange[400] : Colors.grey[200], shape: BoxShape.circle),
+                      ),
+                      Text('جاري تحضير الطلب',
+                          style: TextStyle(fontFamily: 'Droid')),
+                    ],
+                  ),
+                  Container(
+                    color: Colors.grey,
+                    child: Container(
+                      width: 40,
+                      height: 1,
+                      padding: EdgeInsets.all(10),
                     ),
-                    Text('تم استلام طلبك',
-                        style: TextStyle(fontFamily: 'Droid')),
-                  ],
-                ),
-              ],
-            ),
-            Divider(),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'تفاصيل الطلب',
-                  style: TextStyle(fontFamily: 'Droid', fontSize: 22),
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(20.0),
+                        margin: EdgeInsets.all(10),
+                        child: Icon(Icons.done),
+                        decoration: BoxDecoration(
+                            color: orderDetails.level == 'new' || orderDetails.level == 'cooking'|| orderDetails.level == 'delivery'  ? Colors.orange[400] : Colors.grey[200], shape: BoxShape.circle),
+                      ),
+                      Text('تم استلام طلبك',
+                          style: TextStyle(fontFamily: 'Droid')),
+                    ],
+                  ),
+                ],
+              ),
+              Divider(),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'تفاصيل الطلب',
+                    style: TextStyle(fontFamily: 'Droid', fontSize: 22),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: orderDetails.orderItems.length,
-                    itemBuilder: (context, index) {
+              Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: orderDetails.orderItems.length,
+                      itemBuilder: (context, index) {
 
-                      return Container(
-                        color: Colors.grey[100],
-                        margin: EdgeInsets.only(top: 10,bottom: 10),
-                        padding: EdgeInsets.all(10),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              width: 100,
-                              height: 100,
-                              color: Colors.red,
-                              child: Image.network(orderDetails.orderItems[index].productItem.image),
-                            ),
-                            orderDetails.orderItems[index].productItem.addons.length > 0 ? Container(
-                              width: 50,
-                              child: ListView.builder(
-                                  shrinkWrap: true,
-                                  itemCount: orderDetails.orderItems[index].productItem.addons.length,
-                                  itemBuilder: (context, i) {
-                                    return Text(orderDetails.orderItems[index].productItem.addons[i].name, style: TextStyle(fontFamily: 'Droid'),);
-                                  }),
-                            ) :  Text('لاتوجد اضافات', style: TextStyle(fontFamily: 'Droid'),),
-                            Text(orderDetails.orderItems[index].productItem.name, style: TextStyle(fontFamily: 'Droid'),),
-                          ],
-                        ),
-                      );
-                    })),
-          ],
-        ),
+                        return Container(
+                          color: Colors.grey[100],
+                          margin: EdgeInsets.only(top: 10,bottom: 10),
+                          padding: EdgeInsets.all(10),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                width: 100,
+                                height: 100,
+                                color: Colors.red,
+                                child: Image.network(orderDetails.orderItems[index].productItem.image),
+                              ),
+                              orderDetails.orderItems[index].productItem.addons.length > 0 ? Container(
+                                width: 50,
+                                child: ListView.builder(
+                                    shrinkWrap: true,
+                                    itemCount: orderDetails.orderItems[index].productItem.addons.length,
+                                    itemBuilder: (context, i) {
+                                      return Text(orderDetails.orderItems[index].productItem.addons[i].name, style: TextStyle(fontFamily: 'Droid'),);
+                                    }),
+                              ) :  Text('لاتوجد اضافات', style: TextStyle(fontFamily: 'Droid'),),
+                              Text(orderDetails.orderItems[index].productItem.name, style: TextStyle(fontFamily: 'Droid'),),
+                            ],
+                          ),
+                        );
+                      })),
+            ],
+          ),
+        ),]
       ),
     );
   }
